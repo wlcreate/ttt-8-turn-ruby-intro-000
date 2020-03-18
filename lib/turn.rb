c8e7892d8232b017eb90b578f0d1e2c1db02af94
Waverley Leung <waverley.nyc@gmail.com>
2020-03-18 16:13:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,12 +34,4 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    player_move(board, index, current_player(board))
-    display_board(board)
-  else
-    turn(board)
-  end
 end
